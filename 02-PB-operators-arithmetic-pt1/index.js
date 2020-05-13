40f1d4ffd96c30325e772723fb2ecd59a265448f
vinc86 <vincenzomancuso86@gmail.com>
2020-05-13 12:12:04 +0200

    solved

---
+++ 02-PB-operators-arithmetic-pt1/index.js
@@ -0,0 +1,61 @@
+/* TASK 0 */
+
+let x = 3;
+let y = 2;
+
+console.log(`X = ${x}, Y = ${y}`);
+
+/* TASK 1 */
+
+console.log(`${x} + ${y} = `, x + y);
+
+/* TASK 2 */
+
+console.log(`${x} - ${y} = `, x - y);
+
+/* TASK 3 */
+
+console.log(`${x} x ${y} = `, y * x);
+
+/*  TASK 4 */
+
+console.log(`${x} / ${y} = `, y / x);
+
+/*  TASK 5 */
+
+let z = "10";
+const resultOne = x * y - z;
+
+console.log(`(${x} * ${y}) - ${z} = `, resultOne);
+
+/*  TASK 6 */
+
+let a = 15;
+let b = 9;
+
+console.log(`${a} / ${b} = `, a / b);
+
+/*  TASK 7 */
+
+let c = 20;
+const resultTwo = (a + b) * c;
+
+console.log(`(${a} + ${b}) x ${c} = `, resultTwo);
+
+/*  TASK 8 */
+
+console.log(`increment a`, (a += 1));
+
+/*  TASK 9 */
+
+console.log(`decrement a`, (a -= 1));
+
+/*  TASK 10 */
+
+const d = a - b;
+
+console.log(`${d} + ${c} =`, d + c);
+
+/*  TASK 11 */
+
+console.log(`${resultOne} / ${resultTwo} = `, resultOne / resultTwo);
--- PB-language-statements/index.js
+++ PB-language-statements/index.js
@@ -1,4 +1,4 @@
 console.log("Hello World!");
-/* console.log("Thi is some other text..."); */
+/* console.log("This is some other text..."); */
 
 //a syntax error message is displayed : missing ) after argument list
