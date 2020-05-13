9a0ed7355aaa35b67e709b77172557ead7710ed1
vinc86 <vincenzomancuso86@gmail.com>
2020-05-13 14:33:55 +0200

    comments added

--- 02-PB-operators-arithmetic-pt1/index.js
+++ 02-PB-operators-arithmetic-pt1/index.js
@@ -22,40 +22,38 @@ console.log(`${x} x ${y} = `, y * x);
 console.log(`${x} / ${y} = `, y / x);
 
 /*  TASK 5 */
-
+console.log("here");
 let z = "10";
-const resultOne = x * y - z;
+const resultOne = (x * y) / z;
 
-console.log(`(${x} * ${y}) - ${z} = `, resultOne);
-
-/*  TASK 6 */
+console.log(`(${x} * ${y}) / ${z} = `, resultOne);
 
 let a = 15;
 let b = 9;
 
 console.log(`${a} / ${b} = `, a / b);
 
-/*  TASK 7 */
+/*  TASK 6 */
 
 let c = 20;
 const resultTwo = (a + b) * c;
 
 console.log(`(${a} + ${b}) x ${c} = `, resultTwo);
 
-/*  TASK 8 */
+/*  TASK 7 */
 
 console.log(`increment a`, (a += 1));
 
-/*  TASK 9 */
+/*  TASK 8 */
 
 console.log(`decrement a`, (a -= 1));
 
-/*  TASK 10 */
+/*  TASK 9 */
 
 const d = a - b;
 
 console.log(`${d} + ${c} =`, d + c);
 
-/*  TASK 11 */
+/*  TASK 10 */
 
 console.log(`${resultOne} modulo ${resultTwo} = `, resultOne % resultTwo);
