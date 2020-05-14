6601eae006569ea4e9879e6a8768797866afa196
vinc86 <vincenzomancuso86@gmail.com>
2020-05-14 11:40:01 +0200

    solved

---
+++ 03-PB-console/index.js
@@ -0,0 +1,29 @@
+/* task 1 */
+
+console.log("some example text");
+
+/* task 2 */
+
+let value = "This is the value";
+console.log(value);
+
+/* task 3 */
+
+let firstName = "Vincenzo";
+let lastName = "Mancuso";
+
+console.log(firstName, lastName);
+
+/* task 4 */
+
+let name = "John";
+let city = "California";
+let job = "Developer";
+let age = "35";
+let animal = "Dog";
+
+console.log(name, city, job, age, animal);
+
+/* task 5 */
+
+console.log(`Name: ${name}, City: ${city}, Job: ${job}, Animals: ${animal}`);
