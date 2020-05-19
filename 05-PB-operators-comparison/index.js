f54e415ef8bf45341d30794dcd559bbdf5a7da24
vinc86 <vincenzomancuso86@gmail.com>
2020-05-19 21:35:44 +0200

    done

--- 05-PB-operators-comparison/index.js
+++ 05-PB-operators-comparison/index.js
@@ -39,3 +39,44 @@ let second = y - z;
 let result = first > second ? "well done" : "wrong operator, try with ===";
 
 console.log("task 10: ", result);
+("use strict");
+
+// task 0
+let x = 20;
+let y = 30;
+console.log("task 0: ", x, y);
+
+// task 1
+console.log("task 1: ", x === y);
+
+// task 2
+console.log("task 2: ", x !== y);
+
+// task 3
+console.log("task 3: ", x > y);
+
+// task 4
+console.log("task 4: ", x <= y);
+
+// task 5
+console.log("task 5: ", y > x);
+
+// task 6
+let z = 5;
+console.log("task 6: ", z * x > z + y);
+
+// task 7
+console.log("task 7: ", x - z < y / z);
+
+// task 8
+console.log("task 8: ", z === x && z === y && x === y);
+
+// task 9
+console.log("task 9: ", x % z === y % z);
+
+// task 10
+let first = z + x;
+let second = y - z;
+let result = first > second ? "well done" : "wrong operator, try with ===";
+
+console.log("task 10: ", result);
---
+++ 09-PB-language-number/index.js
@@ -0,0 +1,18 @@
+// task 1
+
+let stringOne = "This is a string ";
+let numberOne = 40;
+
+let result = stringOne + numberOne;
+console.log(`Task 1 : ${result}`);
+
+// task 2
+
+let valueOne = "1005";
+let valueTwo = "10.05";
+
+console.log(`Task 2: ${parseInt(valueOne)} ${parseFloat(valueTwo)}`); // test purpose // task 3
+/* console.log(typeof parseInt(valueOne)); */ console.log("Task 3:");
+console.log(30 % 2 === 0 ? "----> 30 is even" : "----> 30 is odd");
+console.log(939 % 2 === 0 ? "----> 939 is even" : "----> 939 is odd");
+console.log(40.9 % 2 === 0 ? "----> 40.9 is even" : "----> 40.9 is odd");
