ca79e88b6c11a27d3f9748291512c2e7dbc6f7a9
vinc86 <vincenzomancuso86@gmail.com>
2020-05-19 11:36:34 +0200

    02 fixed

--- 02-PB-operators-arithmetic-pt1/index.js
+++ 02-PB-operators-arithmetic-pt1/index.js
@@ -1,6 +1,3 @@
-
-
-<<<<<<< HEAD
 /* TASK 0 */
 
 let x = 3;
@@ -59,55 +56,3 @@ console.log(`${d} + ${c} =`, d + c);
 /*  TASK 10 */
 
 console.log(`${resultOne} modulo ${resultTwo} = `, resultOne % resultTwo);
-=======
-// 0.
-let x = 6;
-console.log(x, typeof x);
-let y = 7;
-console.log(y, typeof y);
-
-// 1.
-console.log(x + y);
-
-// 2.
-console.log(x - y);
-console.log(y - x);
-
-// 3.
-console.log(x * y);
-
-// 4.
-console.log(x / y);
-
-// 5.
-let z = 10;
-console.log(z, typeof z);
-let resultOne = (x * y) / z;
-console.log(resultOne);
-
-// 6.
-let a = 15;
-let b = 9;
-console.log(a / b);
-
-// 7.
-let c = 20;
-console.log(c, typeof c);
-let resultTwo = (a + b) * c;
-console.log(resultTwo);
-
-// 8.
-console.log(++a);
-
-// 9.
-console.log(--b);
-
-
-// 10. my question: the answer should be 14 but why is the machine calculation result 12?
-let d = b - a;
-console.log(d);
-console.log(d + c);
-
-// 11.
-console.log(resultOne % resultTwo);
->>>>>>> 2b144e551a9dffbf5d09cf3e123c1b016f750e5b
