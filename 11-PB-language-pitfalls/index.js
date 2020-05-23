ffec06446824bb8826ea1accf1f45a4581a13725
vinc86 <vincenzomancuso86@gmail.com>
2020-05-23 18:36:06 +0200

    done

---
+++ 11-PB-language-pitfalls/index.js
@@ -0,0 +1,18 @@
+// task 1
+
+// NaN stand for: Not a Number
+
+console.log(`Task 1a: ${isNaN("Hello")}`);
+console.log(`Task 1b: ${isNaN(3)}`);
+console.log(`Task 1c: ${typeof NaN}`);
+
+// task 2
+
+let result = 0.1 * 0.2;
+console.log(`Task 2: ${result.toFixed(2)}`);
+
+// task 3
+
+console.log(`Task 3a: ${Infinity / 0}`);
+console.log(`Task 3b: ${Infinity / Infinity}`);
+console.log(`Task 3c: ${1 / 0}`);
