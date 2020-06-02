c12f969da4373805f5fbe1640b19f2e47d2eb3e3
vinc86 <vincenzomancuso86@gmail.com>
2020-06-02 15:33:59 +0200

    21 done

---
+++ 21-PB-program-repetition2/index.js
@@ -0,0 +1,33 @@
+// task 1
+console.log("Task 1:");
+let i = 0;
+
+while (i < 15) {
+    console.log(i);
+    i++;
+}
+
+
+// task 2
+
+console.log("Task 2:");
+let count = 1;
+let sum = 0;
+while (count <= 20) {
+    sum = sum + count;
+    count++;
+}
+
+console.log(sum);
+
+
+// task 3
+
+console.log("Task 3:");
+
+let num = 0;
+
+do {
+    num++;
+    console.log(`The number is ${num}`);
+} while (num < 20);
