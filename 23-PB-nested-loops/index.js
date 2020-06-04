831ed4bc9a66798f8780fdec2fb6afb426879452
vinc86 <vincenzomancuso86@gmail.com>
2020-06-04 11:26:32 +0200

    solved

---
+++ 23-PB-nested-loops/index.js
@@ -0,0 +1,38 @@
+for (let i = 0; i <= 10; i++) {
+    let star = "";
+    for (let l = 10; l > i; l--) {
+        star += " ";
+    }
+    for (let c = 0; c < i * 2 - 1; c++) {
+        star += "*";
+
+    }
+    for (let r = 10; r > i; r--) {
+        star += " ";
+
+    }
+
+    console.log(star);
+}
+
+
+//reverse tree
+
+// i really have no idea how i get it right :D
+
+var n = 5;
+console.log();
+
+for (let i = 0; i <= 20; i++) {
+    let star = ""
+
+    for (let s = 0; s <= i; s++) {
+        star += " ";
+
+    }
+    for (let s = 20; s >= i * 2; s--) {
+        star += "*";
+
+    }
+    console.log(star);
+}
