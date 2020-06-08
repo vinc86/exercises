ca3316209c622f184f9ec48aa183cf02d8aa3237
vinc86 <vincenzomancuso86@gmail.com>
2020-06-08 15:38:00 +0200

    solved

--- 26-PB-function-weekend/index.js
+++ 26-PB-function-weekend/index.js
@@ -2,14 +2,14 @@
 
 const date = new Date("2020-06-07");
 
-const checkWeekend = (date) => {
+const checkWeekend = (value) => {
 
 
-    if (date.getDay() === 6 || date.getDay() === 0) {
+    if (value.getDay() === 6 || value.getDay() === 0) {
         if (date.getDay() === 6) {
-            console.log(`The ${date.getDate()}th of ${date.getMonth()+1} is a Saturday! Enjoy the Weekend!`);
+            console.log(`The ${value.getDate()}th of ${value.getMonth()+1} is a Saturday! Enjoy the Weekend!`);
         } else {
-            console.log(`The ${date.getDate()}th of ${date.getMonth()+1} is a Sunday! The Weekend comes slowly to end D:`);
+            console.log(`The ${value.getDate()}th of ${value.getMonth()+1} is a Sunday! The Weekend comes slowly to end D:`);
         }
     } else {
         console.log("It's not a weekend day! :(");
---
+++ 27-PB-more-on-dates/index.js
@@ -0,0 +1,40 @@
+"use strict";
+
+//1,3,5,7,8,10,12 = 31;
+// 2 = 28;
+//4,6,9,11 = 30;    
+
+const date = new Date("2020-04-8");
+
+const checkDays = (value) => {
+    const month = value.getMonth() + 1;
+    const day = value.getDate();
+    let restDays;
+
+    switch (month) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            restDays = 31 - day;
+            console.log(`${restDays} left till the end of the Month!`);
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            restDays = 30 - day;
+            console.log(`${restDays} left till the end of the Month!`);
+            break;
+        default:
+            restDays = 28 - day;
+            console.log(`${restDays} left till the end of the Month!`);
+            break;
+    }
+
+}
+
+checkDays(date);
