5a38f91d555dc1a519d455c22703eed686ccf1b2
vinc86 <vincenzomancuso86@gmail.com>
2020-06-09 10:36:32 +0200

    solved

---
+++ 28-PB-function-scope/index.js
@@ -0,0 +1,48 @@
+"use strict";
+
+
+console.log("Task 1:");
+(function printNumbers(a, b) {
+    if ((typeof a !== "number") || (typeof b !== "number")) {
+        console.log("Expecting numbers as parameters!");
+    } else {
+        let result = null;
+        for (let i = 1; i <= b; i++) {
+            result = Math.pow(a, i)
+            console.log(result);
+        }
+    }
+
+})(2, 8);
+
+///////////////////////////////////////////
+
+console.log("Task 2:");
+
+let fruit = "peach";
+
+const printFavoriteFruit = (v) => {
+    console.log(`My favorite fruit is: ${v}`);
+
+}
+
+printFavoriteFruit(fruit);
+
+///////////////////////////////////////////
+
+
+console.log("Task 3:");
+
+const exponent = (a, b) => {
+    let result = null;
+    result = a * b;
+    console.log(result);
+}
+
+exponent(2, 4);
+
+/* 
+is not possible to access the result variable outside the function;
+Because it is declared in the function scope.
+
+*/
