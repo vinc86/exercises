d21d2bfc60d97b62dcbb59476c2f5d6608610abd
vinc86 <vincenzomancuso86@gmail.com>
2020-06-24 10:17:09 +0200

    done

--- 37-PB-arrays-nested-loops/index.js
+++ 37-PB-arrays-nested-loops/index.js
@@ -54,7 +54,7 @@ const printNumbers = () => {
     for (let i = 1; i <= 4; i++) {
 
         for (let j = 0; j <= 4; j++) {
-            if (j !== 0 && j <= 3) {
+            if (j !== 0 && j !== 4) {
                 arrayOne.push(i);
             }
             arrayTwo.push(j);
