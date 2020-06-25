cbe66615b2acba432b3e2202257689a9cee089fc
vinc86 <vincenzomancuso86@gmail.com>
2020-06-25 15:46:04 +0200

    num2 done

--- 40-PB-objects-pt2/index.js
+++ 40-PB-objects-pt2/index.js
@@ -40,3 +40,50 @@ checkRange(5, {
 
 // 2. Scrabble
 console.log("\n2. Scrabble:");
+
+const playerSet = [{
+        tile: "N",
+        score: 1
+    },
+    {
+        tile: "K",
+        score: 5
+    },
+    {
+        tile: "Z",
+        score: 10
+    },
+    {
+        tile: "X",
+        score: 8
+    },
+    {
+        tile: "D",
+        score: 2
+    },
+    {
+        tile: "A",
+        score: 1
+    },
+    {
+        tile: "E",
+        score: 1
+    }
+];
+
+const scrabbleScore = (obj) => {
+    let totalScore = 0;
+    for (let first of obj) {
+        for (let elements in first) {
+            if (elements === "score") {
+                let point = first[elements];
+                totalScore += point;
+            };
+        }
+    }
+    return `Total score: ${totalScore} points`;
+}
+
+console.log(scrabbleScore(playerSet));
+
+//////////////////////////////////////////////////
