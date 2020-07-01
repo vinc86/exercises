4cb18e2526827182941878e370313d493febe002
vinc86 <vincenzomancuso86@gmail.com>
2020-07-01 09:12:13 +0200

    edit

--- 43-PB-datastructure-advarray/index.js
+++ 43-PB-datastructure-advarray/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
-// 1.
+// 1. Get total orders
+console.log("1. Get total orders:");
 const orders = [{
         amount: 250
     },
@@ -25,47 +26,74 @@ for (const obj of orders) {
     }
 }
 const totalAmount = arrayOrders.reduce(function (previous, current) {
-            return previous + current;
-        }
-
-        // 2
-        const arrayOfNumbers = [3, 45, 6, 56, 7, 9]; console.log(arrayOfNumbers);
-        const newArrayOfNumbers = arrayOfNumbers.map(function (value) {
-            return value + 1;
-        }); console.log('ex 2.', newArrayOfNumbers);
-
-        // const result = ar.filter(num => num === maxNum);
-        Array.prototype.filter
-        // 3.
-        const filterEvens = (arr) => arr.filter(function (value) {
-            return value % 2 === 0;
-        });
-
-        console.log('ex 3', filterEvens([1, 2, 3, 11, 12, 13])); console.log('ex 3', filterEvens([22, 2, 31, 110, 6, 13]));
+    return previous + current;
+});
 
 
-        // 4. 
-        const friends = ["rika", "jenna", "bleda", "oliver", "itamar"];
-        const filterItems = (arr, str) => {
-            const filteredArr = arr.filter(function (value) {
-                return value.includes(str);
-            })
-            for (let i = 0; i < filteredArr.length; i++) {
-                filteredArr[i] = filteredArr[i][0].toUpperCase() + filteredArr[i].slice(1);
-            }
-            return filteredArr;
-        }
+//////////////////////////////////////////////////
 
-        console.log('ex 4', filterItems(friends, 'ka')); console.log('ex 4', filterItems(friends, 'e'));
+// 2. Increment by 1
+console.log("\n2. Increment by 1:");
 
 
-        // 5. 
-        const sumUp = (arr) => arr.reduce((previous, current) => previous + current);
+const arrayOfNumbers = [3, 45, 6, 56, 7, 9];
+console.log(arrayOfNumbers);
+const newArrayOfNumbers = arrayOfNumbers.map(function (value) {
+    return value + 1;
+});
+console.log('ex 2.', newArrayOfNumbers);
 
-        console.log('ex 5', sumUp([1, 2, 3, 4, 5])); console.log('ex 5', sumUp([6, 7, 7]));
+
+Array.prototype.filter
+
+///////////////////////////////////////////////////
+
+// 3. Filter Evens
+console.log("3. Filter Evens:");
+
+const filterEvens = (arr) => arr.filter(function (value) {
+    return value % 2 === 0;
+});
+
+console.log('ex 3', filterEvens([1, 2, 3, 11, 12, 13]));
+console.log('ex 3', filterEvens([22, 2, 31, 110, 6, 13]));
 
 
-        // 6. 
-        const squareRoot = (arr) => arr.map((value) => Math.sqrt(value));
+///////////////////////////////////////////////////////
 
-        console.log('ex 6', squareRoot([4, 9, 16, 25, 44]));
+// 4. Filter Friends
+console.log("4. Filter Friends:");
+
+
+const friends = ["rika", "jenna", "bleda", "oliver", "itamar"];
+const filterItems = (arr, str) => {
+    const filteredArr = arr.filter(function (value) {
+        return value.includes(str);
+    })
+    for (let i = 0; i < filteredArr.length; i++) {
+        filteredArr[i] = filteredArr[i][0].toUpperCase() + filteredArr[i].slice(1);
+    }
+    return filteredArr;
+}
+
+console.log('ex 4', filterItems(friends, 'ka'));
+console.log('ex 4', filterItems(friends, 'e'));
+
+/////////////////////////////////////////////////////
+
+// 5. Sum Up
+console.log("5. Sum Up:");
+
+const sum3 = arr => arr.reduce((previous, current) => previous + current);
+
+console.log(sum3([1, 2, 3, 4, 5]));
+console.log(sum3([6, 7, 7]));
+
+////////////////////////////////////////////////////
+
+// 6. Square Root
+console.log("6. Square Root:");
+
+const squareRoot = arr => arr.map((value) => Math.sqrt(value));
+
+console.log(squareRoot([4, 9, 16, 25, 44]));
