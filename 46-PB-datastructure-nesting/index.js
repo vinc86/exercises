a590846a94468168e128cc2e430df328d549197b
vinc86 <vincenzomancuso86@gmail.com>
2020-07-03 12:37:22 +0200

    done

--- 46-PB-datastructure-nesting/index.js
+++ 46-PB-datastructure-nesting/index.js
@@ -1,4 +1,6 @@
 "use strict";
+//1. 2D Array
+console.log("\n1. 2D Array:");
 
 let board = [
     [1, 2, 3],
@@ -39,3 +41,57 @@ class PrintArray {
 
 const arrayOfValues = new PrintArray(board);
 console.log(arrayOfValues.getAllNests());
+
+//////////////////////////////////////////////////////
+
+//2. Doggo
+console.log("\n2. Doggo:");
+
+class Doggo {
+    constructor(name, breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+    favoriteFoods = ["kibble", "chicken", "canned goods"];
+    printFavoriteFoods = () => {
+        return Object.values(this.favoriteFoods).join(", ");
+    }
+}
+
+const myDog = new Doggo;
+console.log(myDog.favoriteFoods[1]);
+console.log(myDog.printFavoriteFoods());
+
+///////////////////////////////////////////////////////////
+
+// 3.Recipes
+console.log("\n3.Recipes:");
+
+const recipes = {
+
+    ingredients: [
+        "butter",
+        "sugar",
+        "flour"
+    ],
+
+    printIngredients: function () {
+        return Object.values(this.ingredients).join(", ");
+    }
+}
+recipes.ingredients.push("ginger");
+
+console.log(recipes.ingredients);
+
+const changeIngredientName = (array, originalName, newName) => {
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === originalName) {
+            array[i] = newName;
+        };
+    }
+}
+
+changeIngredientName(recipes.ingredients, "sugar", "brown sugar");
+
+console.log(recipes.printIngredients());
