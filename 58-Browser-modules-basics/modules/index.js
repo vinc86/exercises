687f4aaa3dd88ef53ff0d9c5baaf77baae8eea9c
vinc86 <vincenzomancuso86@gmail.com>
2020-08-03 13:15:22 +0200

    done

---
+++ 58-Browser-modules-basics/modules/index.js
@@ -0,0 +1,13 @@
+import {
+    clickImages
+} from './click.js';
+import {
+    scrollWindow
+} from './scroll.js';
+import {
+    hoverHeader
+} from './hover.js';
+
+clickImages();
+scrollWindow();
+hoverHeader();
